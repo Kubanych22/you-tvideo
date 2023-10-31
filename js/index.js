e4b77a6a53742875ea2fc538226669c42a7abc4b
Kubanych22 <z200707@mail.ru>
2023-10-31 22:12:58 +0600

    Project you-tvideo ready

--- js/index.js
+++ js/index.js
@@ -37,20 +37,20 @@ const convertISOToReadableDuration = (isoDuration) => {
   const hoursMatch = isoDuration.match(/(\d+)H/);
   const minutesMatch = isoDuration.match(/(\d+)M/);
   const secondsMatch = isoDuration.match(/(\d+)S/);
-  
+
   const hours = hoursMatch ? parseInt(hoursMatch[1]) : 0;
   const minutes = minutesMatch ? parseInt(minutesMatch[1]) : 0;
   const seconds = secondsMatch ? parseInt(secondsMatch[1]) : 0;
-  
+
   let result = '';
   if (hours > 0) {
     result += `${hours} ч `;
   }
-  
+
   if (minutes > 0) {
     result += `${minutes} мин `;
   }
-  
+
   if (seconds > 0) {
     result += `${seconds} сек`;
   }
@@ -59,33 +59,33 @@ const convertISOToReadableDuration = (isoDuration) => {
 
 const formatDate = (isoString) => {
   const date = new Date(isoString);
-  
+
   const formatter = new Intl.DateTimeFormat('ru-RU', {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   });
-  
+
   return formatter.format(date);
 };
 
 const fetchTrendingVideos = async () => {
   try {
     const url = new URL(VIDEOS_URL);
-    
+
     url.searchParams.append('part', 'contentDetails,id,snippet');
     url.searchParams.append('chart', 'mostPopular');
     url.searchParams.append('regionCode', 'RU');
     url.searchParams.append('maxResults', '12');
     url.searchParams.append('key', API_KEY);
     const response = await fetch(url);
-    
+
     if (!response.ok) {
       // throw new Error(`HTTP error ${response.status}`);
       console.log(`HTTP error ${response.status}`);
     }
     return await response.json();
-    
+
   } catch (err) {
     console.error('error: ', err);
   }
@@ -97,19 +97,19 @@ const fetchFavoriteVideos = async () => {
       return {items: []};
     }
     const url = new URL(VIDEOS_URL);
-    
+
     url.searchParams.append('part', 'contentDetails,id,snippet');
     url.searchParams.append('maxResults', '12');
     url.searchParams.append('id', favoriteIds.join(','));
     url.searchParams.append('key', API_KEY);
     const response = await fetch(url);
-    
+
     if (!response.ok) {
       // throw new Error(`HTTP error ${response.status}`);
       console.log(`HTTP error ${response.status}`);
     }
     return await response.json();
-    
+
   } catch (err) {
     console.error('error: ', err);
   }
@@ -118,19 +118,19 @@ const fetchFavoriteVideos = async () => {
 const fetchVideoData = async (id) => {
   try {
     const url = new URL(VIDEOS_URL);
-    
+
     url.searchParams.append('part', 'snippet,statistics');
     url.searchParams.append('maxResults', '12');
     url.searchParams.append('id', id);
     url.searchParams.append('key', API_KEY);
     const response = await fetch(url);
-    
+
     if (!response.ok) {
       // throw new Error(`HTTP error ${response.status}`);
       console.log(`HTTP error ${response.status}`);
     }
     return await response.json();
-    
+
   } catch (err) {
     console.error('error: ', err);
   }
@@ -139,23 +139,23 @@ const fetchVideoData = async (id) => {
 const fetchSearchVideos = async (searchQuery, page) => {
   try {
     const url = new URL(SEARCH_URL);
-    
+
     url.searchParams.append('part', 'snippet');
     url.searchParams.append('q', searchQuery);
     url.searchParams.append('type', 'video');
     url.searchParams.append('key', API_KEY);
-    
+
     if (page) {
       url.searchParams.append('pageToken', page);
     }
-    
+
     const response = await fetch(url);
     if (!response.ok) {
       // throw new Error(`HTTP error ${response.status}`);
       console.log(`HTTP error ${response.status}`);
     }
     return await response.json();
-    
+
   } catch (err) {
     console.error('error: ', err);
   }
@@ -166,19 +166,19 @@ const createListVideo = (videos, titleText, pagination) => {
   videoListSection.classList.add('video-list');
   const container = document.createElement('div');
   container.classList.add('container');
-  
+
   const title = document.createElement('h2');
   title.classList.add('video-list__title');
   title.textContent = titleText;
   const videoListItems = document.createElement('ul');
   videoListItems.classList.add('video-list__items');
-  
+
   const listVideos = videos.items.map(video => {
     const li = document.createElement('li');
     li.classList.add('video-list__item');
-    
+
     const id = video.id.videoId || video.id
-    
+
     li.innerHTML = `
       <article class="video-card">
         <a class="video-card__link" href="#/video/${id}">
@@ -202,11 +202,11 @@ const createListVideo = (videos, titleText, pagination) => {
   videoListItems.append(...listVideos);
   videoListSection.append(container);
   container.append(title, videoListItems);
-  
+
   if (pagination) {
     const paginationElem = document.createElement('div');
     paginationElem.classList.add('pagination');
-    
+
     if (pagination.prev) {
       const arrowPrev = document.createElement('a');
       arrowPrev.classList.add('pagination__arrow');
@@ -214,7 +214,7 @@ const createListVideo = (videos, titleText, pagination) => {
       arrowPrev.href = `#/search?q=${pagination.searchQuery}&page=${pagination.prev}`;
       paginationElem.append(arrowPrev);
     }
-    
+
     if (pagination.next) {
       const arrowNext = document.createElement('a');
       arrowNext.classList.add('pagination__arrow');
@@ -225,14 +225,14 @@ const createListVideo = (videos, titleText, pagination) => {
 
     videoListSection.append(paginationElem);
   }
-  
+
   return videoListSection;
 };
 
 const createVideo = (video) => {
   const videoSection = document.createElement('section');
   videoSection.classList.add('video');
-  
+
   videoSection.innerHTML = `
     <div class="container">
       <div class="video__player">
@@ -254,7 +254,7 @@ const createVideo = (video) => {
         <button class="video__link favorite ${favoriteIds.includes(video.id) ? 'active' : ''}">
           <span class="video__no-favorite">Избранное</span>
           <span class="video__favorite">В избранном</span>
-          
+
           <svg class="video__icon">
             <use xlink:href="image/sprite.svg#star-ob"></use>
           </svg>
@@ -262,7 +262,7 @@ const createVideo = (video) => {
       </div>
     </div>
   `;
-  
+
   return videoSection;
 };
 
@@ -278,11 +278,11 @@ const createHero = () => {
             <use xlink:href="image/sprite.svg#star-ow"></use>
           </svg>
         </a>
-        
+
         <svg class="hero__logo" viewBox="0 0 360 48" role="img" aria-label="Логотип сервиса You-Tvideo">
           <use xlink:href="image/sprite.svg#logo-white"></use>
         </svg>
-        
+
         <h1 class="hero__title">Смотри. Загружай. Создавай</h1>
         <p class="hero__tagline">Удобный видеохостинг для тебя</p>
       </div>
@@ -305,7 +305,7 @@ const createHeader = () => {
           <use xlink:href="/image/sprite.svg#logo-orange"></use>
         </svg>
       </a>
-      
+
       <a class="header__link header__link_favorite" href="#/favorite">
         <span class="header__link-text">Избранное</span>
         <svg class="header__icon">
@@ -314,7 +314,7 @@ const createHeader = () => {
       </a>
     </div>
   `;
-  
+
   return headerElem;
 };
 
@@ -322,17 +322,17 @@ const createSearch = () => {
   const searchSection = document.createElement('section');
   const container = document.createElement('div');
   const title = document.createElement('h2');
-  
+
   searchSection.classList.add('search');
   container.classList.add('container');
   title.classList.add('visually-hidden');
   title.textContent = 'Поиск';
-  
+
   const form = document.createElement('form');
   form.classList.add('search__form');
   container.append(title, form);
   searchSection.append(container);
-  
+
   form.innerHTML = `
     <input class="search__input" type="search" name="search" placeholder="Найти видео..." required>
       <button class="search__btn" type="submit">
@@ -348,13 +348,13 @@ const createSearch = () => {
     if (form.search.value.trim()) {
       router.navigate(`/search?q=${form.search.value}`);
     }
-    
+
   });
   return searchSection;
 };
 
 const indexRoute = async () => {
-  document.querySelector('.header').remove();
+  document.querySelector('.header')?.remove();
   main.textContent = '';
   preload.append();
   const hero = createHero();
@@ -371,16 +371,16 @@ const videoRoute = async (ctx) => {
   const id = ctx.data.id;
   main.textContent = '';
   preload.append();
-  
+
   document.body.prepend(createHeader());
-  
+
   const search = createSearch();
   const data = await fetchVideoData(id);
   const video = data.items[0];
   preload.remove();
   const videoSection = createVideo(video);
   main.append(search, videoSection);
-  
+
   const searchQuery = video.snippet.title;
   const videos = await fetchSearchVideos(searchQuery);
   const listVideo = createListVideo(videos, 'Похожие видео');
@@ -392,7 +392,7 @@ const favoriteRoute = async () => {
   main.textContent = '';
   preload.append();
   const search = createSearch();
-  
+
   const videos = await fetchFavoriteVideos();
   preload.remove();
   const listVideo = createListVideo(videos, 'Избранное');
@@ -402,13 +402,13 @@ const favoriteRoute = async () => {
 const searchRoute = async (ctx) => {
   const searchQuery = ctx.params.q;
   const page = ctx.params.page;
-  
+
   if (searchQuery) {
     document.body.prepend(createHeader());
     main.textContent = '';
     preload.append();
     const search = createSearch();
-    
+
     const videos = await fetchSearchVideos(searchQuery, page);
     preload.remove();
     const listVideo = createListVideo(videos, 'Избранное', {
@@ -428,7 +428,7 @@ const init = () => {
     '/favorite': favoriteRoute,
     '/search': searchRoute,
   }).resolve();
-  
+
   document.body.addEventListener('click', ({target}) => {
     const itemFavorite = target.closest('.favorite');
     if (itemFavorite) {
